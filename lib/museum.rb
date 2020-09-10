3c377091ee3028f46827638d962d97d26ff66e85
Michael Foy <foymikek@gmail.com>
2020-09-10 10:06:55 -0600

    Add meseum method recommend_exhibits

--- lib/museum.rb
+++ lib/museum.rb
@@ -9,4 +9,23 @@ class Museum
     @exhibits << exhibit
   end
 
+  def recommend_exhibits(patron)
+    recommend_exhibits = []
+    recommend_exhibit_names =[]
+    exhibit_names = @exhibits.map do |exhibit|
+        exhibit.name
+      end
+      exhibit_names.each do |name|
+        recommend_exhibit_names << name if patron.interests.include?(name)
+      end
+    recommend_exhibit_names.each do |name|
+      @exhibits.each do |exhibit|
+        recommend_exhibits << exhibit if exhibit.name == name
+      end
+
+    end
+    recommend_exhibits
+    # require "pry"; binding.pry
+  end
+
 end
